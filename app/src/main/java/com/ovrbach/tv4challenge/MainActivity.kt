e24db09ed3152cf69b88494b68a8df2f95e4d2ef
Patrik Figura <mr.svkaka@gmail.com>
2020-11-21 12:12:22 +0100

    initial commit

---
+++ app/src/main/java/com/ovrbach/tv4challenge/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ovrbach.tv4challenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ovrbach.tv4challenge.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/ovrbach/tv4challenge/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.ovrbach.tv4challenge.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
